c1b609331026f11cec6c625404663315ee3e3214
MrVengador <cristian.penavillar@gmail.com>
2025-03-05 16:24:24 -0300

    Cambio de idioma

--- app.js
+++ app.js
@@ -21,5 +21,22 @@ document.querySelectorAll('.modal').forEach(function (modal) {
 
 
 
-
-
+function setLanguage(lang) {
+    if (lang === 'en') {
+        // Mostrar contenido en inglés y ocultar contenido en español
+        document.querySelectorAll('[data-en]').forEach(function (element) {
+            element.style.display = 'block';
+        });
+        document.querySelectorAll('[data-es]').forEach(function (element) {
+            element.style.display = 'none';
+        });
+    } else if (lang === 'es') {
+        // Mostrar contenido en español y ocultar contenido en inglés
+        document.querySelectorAll('[data-es]').forEach(function (element) {
+            element.style.display = 'block';
+        });
+        document.querySelectorAll('[data-en]').forEach(function (element) {
+            element.style.display = 'none';
+        });
+    }
+}
